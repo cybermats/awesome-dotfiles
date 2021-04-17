d75ef991a34c33a1dce61ed4fd4df64d8488aa35
Mats Fredriksson <cybermats@gmail.com>
2021-04-17 02:24:12 +0200

    Changing keys: removing gaps, and adding toggling for tags.

--- awesome/keys.lua
+++ awesome/keys.lua
@@ -441,24 +441,6 @@ keys.globalkeys = gears.table.join(
    ),
 
    -- =========================================
-   -- GAP CONTROL
-   -- =========================================
-
-   -- Gap control
-   awful.key({modkey, "Shift"}, "minus",
-      function()
-         awful.tag.incgap(5, nil)
-      end,
-      {description = "increment gaps size for the current tag", group = "gaps"}
-   ),
-   awful.key({modkey}, "minus",
-      function()
-         awful.tag.incgap(-5, nil)
-      end,
-      {description = "decrement gap size for the current tag", group = "gaps"}
-   ),
-
-   -- =========================================
    -- LAYOUT SELECTION
    -- =========================================
 
@@ -592,6 +574,16 @@ for i = 1, 9 do
          end,
          {description = "view tag #"..i, group = "tag"}
       ),
+      -- Toggle tag display.
+      awful.key({ modkey, "Control" }, "#" .. i + 9,
+	 function ()
+	    local screen = awful.screen.focused()
+	    local tag = screen.tags[i]
+	    if tag then
+	       awful.tag.viewtoggle(tag)
+	    end
+	 end,
+	 {description = "toggle tag #" .. i, group = "tag"}),
       -- Move client to tag
       awful.key({modkey, "Shift"}, "#" .. i + 9,
          function()
@@ -603,6 +595,18 @@ for i = 1, 9 do
             end
          end,
          {description = "move focused client to tag #"..i, group = "tag"}
+      ),
+      -- Toggle tag on focused client.
+      awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
+	 function ()
+	    if client.focus then
+	       local tag = client.focus.screen.tags[i]
+	       if tag then
+		  client.focus:toggle_tag(tag)
+	       end
+	    end
+	 end,
+	 {description = "toggle focused client on tag #" .. i, group = "tag"}
       )
    )
 end
