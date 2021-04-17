1f4cf4b3d2008f1873de8a451dfa8b3bf3c6ddd0
Mats Fredriksson <cybermats@gmail.com>
2021-04-17 02:25:05 +0200

    Changing size of bluetooth icon.

--- awesome/widgets/bluetooth.lua
+++ awesome/widgets/bluetooth.lua
@@ -35,7 +35,7 @@ local widget = wibox.widget {
    layout = wibox.layout.align.horizontal
 }
 
-local widget_button = clickable_container(wibox.container.margin(widget, dpi(7), dpi(7), dpi(7), dpi(7)))
+local widget_button = clickable_container(wibox.container.margin(widget, dpi(5), dpi(5), dpi(5), dpi(5)))
 widget_button:buttons(
    gears.table.join(
       awful.button({}, 1, nil,
